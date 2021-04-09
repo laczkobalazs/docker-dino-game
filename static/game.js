5c84905a512eb3075d28a1278993f471b8198809
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-09 18:30:38 +0200

    adds dino tree collision detection

--- static/game.js
+++ static/game.js
@@ -15,8 +15,15 @@ function jump() {
 let isAlive = setInterval(() => {
   // current dino Y pos
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+
   // current cactus X pos
-  let cactusTop = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+  // collision detection
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    console.log("collision")
+    alert("Game Over")
+  }
 }, 10);
 
 document.addEventListener("keydown", function (event) {
