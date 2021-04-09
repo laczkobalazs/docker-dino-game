0a4c19ec8400c14aa40bf7bd1baa8b0e0fcca0d1
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-09 18:25:50 +0200

    adds func to catch dino Y and cacti Y pos

--- static/game.js
+++ static/game.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
 function jump() {
   if (dino.classList != "jump") {
@@ -12,7 +13,10 @@ function jump() {
 }
 
 let isAlive = setInterval(() => {
-  console.log("check")
+  // current dino Y pos
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+  // current cactus X pos
+  let cactusTop = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 }, 10);
 
 document.addEventListener("keydown", function (event) {
