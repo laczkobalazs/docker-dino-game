239a02b83c1ebadaa1ef052fba43dbd0dab40a6f
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-09 15:49:02 +0200

    adds func to prevent premature jump, so we cannot reset and reinitiate an unfinished jumping animation

--- static/game.js
+++ static/game.js
@@ -1,8 +1,10 @@
 const dino = document.getElementById("dino");
 
 function jump() {
-  dino.classList.add("jump");
-
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+  }
+  
   setTimeout(function () {
     dino.classList.remove("jump");
 
